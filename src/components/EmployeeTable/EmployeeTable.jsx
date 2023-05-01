0c2bde4a231cdba5997a39c658f861221d5bab27
Johan Peraldi <johan.peraldi@gmail.com>
2023-05-01 17:46:07 +0300

    Remove Container component

--- src/components/EmployeeTable/EmployeeTable.jsx
+++ src/components/EmployeeTable/EmployeeTable.jsx
@@ -18,44 +18,42 @@ function EmployeeTable() {
   }, [employees.length, setEmployees]);
 
   return (
-    <Container fluid>
-      <table className={styles.table}>
-        <thead className={styles.head}>
-          <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Start Date</th>
-            <th>Department</th>
-            <th>Date of Birth</th>
-            <th>Street</th>
-            <th>City</th>
-            <th>State</th>
-            <th>Zip Code</th>
-          </tr>
-        </thead>
-        <tbody className={styles.body}>
-          {employees.length > 0 &&
-            employees.map((employee) => (
-              <tr key={employee.id} className={styles.row}>
-                <td>{employee.firstName}</td>
-                <td>{employee.lastName}</td>
-                <td>{employee.startDate}</td>
-                <td>{employee.department}</td>
-                <td>{employee.dateOfBirth}</td>
-                <td>{employee.address.street}</td>
-                <td>{employee.address.city}</td>
-                <td>{employee.address.state}</td>
-                <td>{employee.address.zip}</td>
-              </tr>
-            ))}
-          {employees.length === 0 && (
-            <tr className={styles.row}>
-              <td colSpan="9">No data available in table</td>
+    <table className={styles.table}>
+      <thead className={styles.head}>
+        <tr>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Start Date</th>
+          <th>Department</th>
+          <th>Date of Birth</th>
+          <th>Street</th>
+          <th>City</th>
+          <th>State</th>
+          <th>Zip Code</th>
+        </tr>
+      </thead>
+      <tbody className={styles.body}>
+        {employees.length > 0 &&
+          employees.map((employee) => (
+            <tr key={employee.id} className={styles.row}>
+              <td>{employee.firstName}</td>
+              <td>{employee.lastName}</td>
+              <td>{employee.startDate}</td>
+              <td>{employee.department}</td>
+              <td>{employee.dateOfBirth}</td>
+              <td>{employee.address.street}</td>
+              <td>{employee.address.city}</td>
+              <td>{employee.address.state}</td>
+              <td>{employee.address.zip}</td>
             </tr>
-          )}
-        </tbody>
-      </table>
-    </Container>
+          ))}
+        {employees.length === 0 && (
+          <tr className={styles.row}>
+            <td colSpan="9">No data available in table</td>
+          </tr>
+        )}
+      </tbody>
+    </table>
   );
 }
 
